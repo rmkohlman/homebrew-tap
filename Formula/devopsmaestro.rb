98ae46cbb8bc4e1ae8c76c6d07bad50c292ba87e
rmkohlman <45956230+rmkohlman@users.noreply.github.com>
2026-01-29 15:19:42 -0700

    Add devopsmaestro formula (alias for dvm)

---
+++ Formula/devopsmaestro.rb
@@ -0,0 +1,77 @@
+# typed: false
+# frozen_string_literal: true
+
+class Devopsmaestro < Formula
+  desc "DevOpsMaestro (dvm) - kubectl-style CLI for containerized dev environments"
+  homepage "https://github.com/rmkohlman/devopsmaestro"
+  version "0.3.2"
+  license "GPL-3.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.2/dvm-darwin-arm64"
+      sha256 "69ff95ecad57ae0f30ea48291caa7df627824fe01c26516e043f7edd62ad08f8"
+    end
+    on_intel do
+      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.2/dvm-darwin-amd64"
+      sha256 "61c84b28926f83125a9fb435874511f64e1254157b32ef9d580508636357892e"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.2/dvm-linux-arm64"
+      sha256 "1b71d9800e40b17989055f316dad5ddad4ee7d3e7f142d9252242f293934f2ca"
+    end
+    on_intel do
+      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.2/dvm-linux-amd64"
+      sha256 "e70869cfae8f77ebe2bf43920fd12354927ac13db2decb57c7a3cb31d779f27f"
+    end
+  end
+
+  head "https://github.com/rmkohlman/devopsmaestro.git", branch: "main"
+
+  depends_on "go" => :build if build.head?
+
+  def install
+    if build.head?
+      system "go", "build",
+             "-ldflags", "-s -w -X main.Version=HEAD -X main.Commit=#{`git rev-parse --short HEAD`.strip} -X main.BuildTime=#{Time.now.utc.iso8601}",
+             "-o", bin/"dvm",
+             "."
+    else
+      if OS.mac?
+        if Hardware::CPU.arm?
+          bin.install "dvm-darwin-arm64" => "dvm"
+        else
+          bin.install "dvm-darwin-amd64" => "dvm"
+        end
+      elsif OS.linux?
+        if Hardware::CPU.arm?
+          bin.install "dvm-linux-arm64" => "dvm"
+        else
+          bin.install "dvm-linux-amd64" => "dvm"
+        end
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To get started:
+        dvm admin init
+        dvm create project myproject --from-cwd
+
+      For shell completions, run:
+        # Zsh
+        dvm completion zsh > $(brew --prefix)/share/zsh/site-functions/_dvm
+        
+        # Bash  
+        dvm completion bash > $(brew --prefix)/etc/bash_completion.d/dvm
+    EOS
+  end
+
+  test do
+    assert_match "dvm", shell_output("#{bin}/dvm --help")
+  end
+end
