387fa0d3c6f4b768f357d1bb8f10935114166ee2
rmkohlman <45956230+rmkohlman@users.noreply.github.com>
2026-02-03 21:33:27 -0700

    devopsmaestro 0.7.0

--- Formula/devopsmaestro.rb
+++ Formula/devopsmaestro.rb
@@ -4,36 +4,18 @@
 class Devopsmaestro < Formula
   desc "DevOpsMaestro (dvm) - kubectl-style CLI for containerized dev environments"
   homepage "https://github.com/rmkohlman/devopsmaestro"
-  version "0.3.3"
+  version "0.7.0"
   license "GPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.3/devopsmaestro_0.3.3_darwin_arm64.tar.gz"
-      sha256 "b9d5117c5ab4cdff2e777907fd0f240e155a43c0e1b02d9767d934e5515f3a13"
+      url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.7.0/devopsmaestro_0.7.0_darwin_arm64.tar.gz"
+      sha256 "3d28982d34c27d479ffda11367fcfe3ed2a5ba157d20f53e77ec58326ef902f8"
     end
-    # Note: darwin/amd64 not currently built - requires cross-compilation
-    # on_intel do
-    #   url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.3/devopsmaestro_0.3.3_darwin_amd64.tar.gz"
-    #   sha256 "PLACEHOLDER_DARWIN_AMD64"
-    # end
   end
-
-  # Note: Linux builds not currently available - requires cross-compilation toolchains
-  # on_linux do
-  #   on_arm do
-  #     url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.3/devopsmaestro_0.3.3_linux_arm64.tar.gz"
-  #     sha256 "PLACEHOLDER_LINUX_ARM64"
-  #   end
-  #   on_intel do
-  #     url "https://github.com/rmkohlman/devopsmaestro/releases/download/v0.3.3/devopsmaestro_0.3.3_linux_amd64.tar.gz"
-  #     sha256 "PLACEHOLDER_LINUX_AMD64"
-  #   end
-  # end
 
   head "https://github.com/rmkohlman/devopsmaestro.git", branch: "main"
 
-  # Both formulas install the same 'dvm' binary
   conflicts_with "dvm", because: "both install the dvm binary"
 
   depends_on "go" => :build if build.head?
@@ -44,13 +26,9 @@ class Devopsmaestro < Formula
              "-ldflags", "-s -w -X main.Version=HEAD -X main.Commit=#{`git rev-parse --short HEAD`.strip} -X main.BuildTime=#{Time.now.utc.iso8601}",
              "-o", bin/"dvm",
              "."
-      # Generate completions when building from source (not sandboxed)
       generate_completions_from_executable(bin/"dvm", "completion")
     else
-      # Install binary from archive
       bin.install "dvm"
-
-      # Install pre-generated completions from archive
       bash_completion.install "completions/dvm.bash" => "dvm"
       zsh_completion.install "completions/_dvm"
       fish_completion.install "completions/dvm.fish"
